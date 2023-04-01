20342ce4ac0c69b7b0b02893e260272745f627af
shaimeur <haimeursaad@gmail.com>
2023-04-01 21:52:05 +0000

    using arrow function with the event.currentTarget to correct the this keyword reference separating the event with function

--- 05-Flex-Panel-Gallery/index.js
+++ 05-Flex-Panel-Gallery/index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+let panels = document.querySelectorAll(".panel");
+
+const toggleOpen = (event) => {
+  return event.currentTarget.classList.toggle("open");
+};
+
+const  toggleActive = (event) =>{
+    console.log(event.propertyName) ;
+    // return event.currentTarget.include ;
+    if(event.propertyName.includes('flex')){
+        event.currentTarget.classList.toggle("open-active")
+    }
+}
+
+panels.forEach(panel => panel.addEventListener("click",toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend",toggleActive));
