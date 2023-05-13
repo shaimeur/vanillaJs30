53d14b53a979088ac1f2877a3c9a5f71a3f685c5
shaimeur <haimeursaad@gmail.com>
2023-05-13 18:11:31 +0100

    solving some basic shit

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -1,5 +1,4 @@
  // ## Array Cardio Day 2
-console.log("eeeeeeeeeeeee")
  const people = [
     { name: 'Wes', year: 1988 },
     { name: 'Kait', year: 1986 },
@@ -16,13 +15,40 @@ console.log("eeeeeeeeeeeee")
   ];
 
   // Some and Every Checks
+  const date = new Date();
+  const year = date.getFullYear();
   // Array.prototype.some() // is at least one person 19 or older?
-  // Array.prototype.every() // is everyone 19 or older?
 
+  const isAdult = ()=>{
+    return  people.some(person =>{
+      return year - person.year >= 19;
+    })
+  }
+  console.log(isAdult())
+    // Array.prototype.every() // is everyone 19 or older?
+
+  const isAllAdult = ()=>{
+    return people.every(person=>{
+      return year - person.year >=19 ;
+    })
+  }
+  console.log(isAllAdult())
   // Array.prototype.find()
+
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
-
+  const findComment = ()=>{
+    return comments.find(comment =>{
+      return comment.id === 823423;
+    })
+  }
+  console.log(findComment())
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+  const index = comments.findIndex(comment =>{
+    return comment.id === 823423;
+  })
+  console.log(index)
+  comments.splice(index,1)
+  console.table(comments)
